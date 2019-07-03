24e6e268402c7198c4b9b10d8280f972c189f0dc
lucasgontijo <lucasgontijo0@gmail.com>
2019-07-03 18:35:46 -0300

    First commit.

---
+++ src/main/java/com/example/demospringsoap/DemoSpringSoapApplication.java
@@ -0,0 +1,13 @@
+package com.example.demospringsoap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringSoapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringSoapApplication.class, args);
+    }
+
+}
